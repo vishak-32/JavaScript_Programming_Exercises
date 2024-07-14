89aaa35415bded12afbf3979c506193227957b14
vishak-32 <spvishak8@gmail.com>
2024-07-14 20:37:04 +0530

    Update Ch4_pgm_03.js

--- 04_JS_Functions/Ch4_pgm_03.js
+++ 04_JS_Functions/Ch4_pgm_03.js
@@ -3,18 +3,23 @@
 var sale1;
 var sale2;
 var sale3;
+var sale4;
 
 sale1 = { price: 140, taxRate: 15 };
 sale2 = { price: 40, taxRate: 10 };
 sale3 = { price: 120, taxRate: 20 };
+sale4 = { price: 200, taxRate: 30 };
 
 sale1.tax = sale1.price * sale1.taxRate / 100;
 sale2.tax = sale2.price * sale2.taxRate / 100;
 sale3.tax = sale3.price * sale3.taxRate / 100;
+sale4.tax = sale4.price * sale4.taxRate / 100;
 
 sale1.total = sale1.price + sale1.tax;
 sale2.total = sale2.price + sale2.tax;
 sale3.total = sale3.price + sale3.tax;
+sale4.total = sale4.price + sale4.tax;
+
 
 console.log("price = $" + sale1.price);
 console.log("tax @ " + sale1.taxRate + "% = $" + sale1.tax);
@@ -27,6 +32,10 @@ console.log("total cost = $" + sale2.total);
 console.log("price = $" + sale3.price);
 console.log("tax @ " + sale3.taxRate + "% = $" + sale3.tax);
 console.log("total cost = $" + sale3.total);
+
+console.log("price = $" + sale4.price);
+console.log("tax @ " + sale4.taxRate + "% = $" + sale4.tax);
+console.log("total cost = $" + sale4.total);
 
 
 
