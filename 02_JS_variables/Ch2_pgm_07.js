826b6af92ee242f8e5a6d47db2ee9336e1680411
vishak-32 <spvishak8@gmail.com>
2024-07-14 19:23:03 +0530

    Update Ch2_pgm_07.js

--- 02_JS_variables/Ch2_pgm_07.js
+++ 02_JS_variables/Ch2_pgm_07.js
@@ -2,9 +2,9 @@
 
 var playerName = "Kandra";
 var locationName = "The Dungeon of Doom";
+var health=150;
 
-console.log(playerName + " is in " + locationName);
-
+console.log(playerName +" has health "+" "+health+" "+" and is in " + locationName);
 
 
 /* Further Adventures
