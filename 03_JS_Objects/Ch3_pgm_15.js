942d5c09b9de531d16bc8817cdd8137280949735
vishak-32 <spvishak8@gmail.com>
2024-07-14 20:33:21 +0530

    Update Ch3_pgm_15.js

--- 03_JS_Objects/Ch3_pgm_15.js
+++ 03_JS_Objects/Ch3_pgm_15.js
@@ -1,11 +1,24 @@
 // A player object
 
 var player;
+var player1;
 
 player = {
     name: "Kandra",
     health: 50,
     place: "The Dungeon of Doom",
+    items: "a rusty key, The Sword of Destiny, a piece of cheese"
+};
+
+console.log(player.name);
+console.log(player.name + " is in " + player.place);
+console.log(player.name + " has health " + player.health);
+console.log("Items: " + player.items);
+
+player1 = {
+    name: "Ram",
+    health: 75,
+    place: "Kerala",
     items: "a rusty key, The Sword of Destiny, a piece of cheese"
 };
 
