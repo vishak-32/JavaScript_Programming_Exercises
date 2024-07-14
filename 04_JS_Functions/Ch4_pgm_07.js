a023190c79782b05f52681c9e945e28db5b184fd
vishak-32 <spvishak8@gmail.com>
2024-07-14 20:38:22 +0530

    Update Ch4_pgm_07.js

--- 04_JS_Functions/Ch4_pgm_07.js
+++ 04_JS_Functions/Ch4_pgm_07.js
@@ -2,16 +2,18 @@
 
 var number1 = 1000;
 var number2 = 66;
+var number3=77;
 var result;
 var findTotal;
 
 findTotal = function () {
-	result = number1 + number2;
+	result = number1 + number2 + number3;
 };
 
 findTotal();
 
-console.log(number1 + " + " + number2 + " = " + result);
+console.log(number1 + " + " + number2  + " + " + number3 + " = " + result);
+
 
 
 
