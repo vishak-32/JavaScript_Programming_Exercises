2dbbfe5a10c43910ea515942be3779dd12a3779e
vishak-32 <spvishak8@gmail.com>
2024-07-14 20:37:47 +0530

    Update Ch4_pgm_05.js

--- 04_JS_Functions/Ch4_pgm_05.js
+++ 04_JS_Functions/Ch4_pgm_05.js
@@ -3,18 +3,29 @@
 var findTotal;
 var displayMenu;
 
+var number1;
+var number2;
+var result;
+
+number1 = 10;
+number2 = 20;
+
 findTotal = function () {
-	result = number1 + number2;
+    result = number1 + number2;
 };
 
 displayMenu = function () {
-	console.log("Please choose an option:");
-	console.log("(1) Print log");
-	console.log("(2) Upload file");
-	console.log("(9) Quit");
+    console.log("Please choose an option:");
+    console.log("(1) Print log");
+    console.log("(2) Upload file");
+    console.log("(9) Quit");
 };
 
+findTotal();
 
+console.log("The total is: " + result);
+
+displayMenu();
 
 /* Further Adventures
  *
