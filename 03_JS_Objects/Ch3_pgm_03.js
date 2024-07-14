53240482d11ec55d869edc442a2c43b0aed0f24b
vishak-32 <spvishak8@gmail.com>
2024-07-14 20:17:04 +0530

    Update Ch3_pgm_03.js

--- 03_JS_Objects/Ch3_pgm_03.js
+++ 03_JS_Objects/Ch3_pgm_03.js
@@ -1,15 +1,23 @@
 // A book as an object
 
 var book;
+var book2;
 
 book = {
-    title : "The Hobbit",
+    title : "The Hobbit, or There and Back Again",
     author : "J. R. R. Tolkien",
     published : 1937
 };
 
 console.log(book);
 
+book2={
+    title:"Brave New World",
+    author:"Aldous Huxley",
+    published:1994
+};
+
+console.log(book2);
 
 
 /* Further Adventures
