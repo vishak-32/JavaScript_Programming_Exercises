3a1b146d25bee97f5fca9a31cc9cb8f103f5ff26
vishak-32 <spvishak8@gmail.com>
2024-07-14 20:36:10 +0530

    Update Ch4_pgm_01.js

--- 04_JS_Functions/Ch4_pgm_01.js
+++ 04_JS_Functions/Ch4_pgm_01.js
@@ -1,11 +1,25 @@
 // Displaying an object's properties on the console
 
 var movie1;
+var movie2;
+var blogPost;
 
 movie1 = {
   title: "Inside Out",
   actors: "Amy Poehler, Bill Hader",
   directors: "Pete Doctor, Ronaldo Del Carmen"
+};
+
+movie2 = {
+  title: "The GodFather",
+  actors: "Marlon Brando,Al Pacino",
+  directors: "	Francis Ford Coppola, Ronaldo Del Carmen"
+};
+
+blogPost={
+  Language: "Javascript",
+  Trainer: "Smith",
+  Date: "25-06-2024"
 };
 
 console.log("Movie information for " + movie1.title);
@@ -14,7 +28,17 @@ console.log("Actors: " + movie1.actors);
 console.log("Directors: " + movie1.directors);
 console.log("------------------------------");
 
+console.log("Movie information for "+ movie2.title);
+console.log("------------------------------");
+console.log("Actors: " + movie2.actors);
+console.log("Directors: " + movie2.directors);
+console.log("------------------------------");
 
+console.log("Language info for "+ blogPost.Language);
+console.log("------------------------------");
+console.log("Actors: " + blogPost.Trainer);
+console.log("Directors: " + blogpost.Date);
+console.log("------------------------------");
 
 /* Further Adventures
  *
