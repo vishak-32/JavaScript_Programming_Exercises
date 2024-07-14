1cb001efb5e75c2d67cc6871c532be24b3b00de0
vishak-32 <spvishak8@gmail.com>
2024-07-14 19:26:34 +0530

    Update Ch2_pgm_08.js

--- 02_JS_variables/Ch2_pgm_08.js
+++ 02_JS_variables/Ch2_pgm_08.js
@@ -9,7 +9,10 @@ score = score + 50;
 console.log("New score: " + score);
 
 console.log("Way to go!");
-
+console.log("You have successfully splatted");
+score=score+100;
+console.log(score);
+console.log("Congratulations");
 
 
 /* Further Adventures
