06fc4daa1dfe4530c4cbadfe32482c5a19e8c5a5
vishak-32 <spvishak8@gmail.com>
2024-07-14 20:25:54 +0530

    Update Ch3_pgm_09.js

--- 03_JS_Objects/Ch3_pgm_09.js
+++ 03_JS_Objects/Ch3_pgm_09.js
@@ -2,6 +2,7 @@
 
 var book1;
 var book2;
+var book3;
 
 book1 = {
 	title: "The Hobbit",
@@ -13,9 +14,14 @@ book2 = {
 	author: "Philip Pullman"
 };
 
+book3={
+	title:"The Sun Also Rises",
+	author:"Ernest Hemingway"
+};
+
 console.log(book1.title + " by " + book1.author);
 console.log(book2.title + " by " + book2.author);
-
+console.log(book3.title + " by " + book3.author);
 
 
 /* Further Adventures
