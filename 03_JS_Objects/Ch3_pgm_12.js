b6c5bcdad1a98440ed83d434dc6430172c15d781
vishak-32 <spvishak8@gmail.com>
2024-07-14 20:31:44 +0530

    Update Ch3_pgm_12.js

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -8,7 +8,24 @@ var post = {
 	body : "You will not believe where I just woke up!! Only on a comet..."
 };
 
+console.log("Post Title: " + post.title);
+console.log("Author: " + post.author);
+console.log("Created Date: " + post.created);
+console.log("Body:\n" + post.body);
 
+
+var post1={
+	id:2,
+	title:"A Suitable Boy",
+	author: "Vikram Seth",
+        created:"1983-04-21",
+	body:"You will not believe where I just woke up!! Only on a comet..."
+};
+
+console.log("Post Title: " + post1.title);
+console.log("Author: " + post1.author);
+console.log("Created Date: " + post1.created);
+console.log("Body:\n" + post1.body);
 
 /* Further Adventures
  *
