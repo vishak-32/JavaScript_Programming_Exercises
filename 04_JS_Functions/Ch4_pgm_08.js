22215921de81cc6b0f535a27587541b76f5d42fa
vishak-32 <spvishak8@gmail.com>
2024-07-14 20:38:43 +0530

    Update Ch4_pgm_08.js

--- 04_JS_Functions/Ch4_pgm_08.js
+++ 04_JS_Functions/Ch4_pgm_08.js
@@ -6,12 +6,25 @@ displayMenu = function () {
 	console.log("Please choose an option:");
 	console.log("(1) Print log");
 	console.log("(2) Upload file");
+	console.log("(3) Download ");
+        console.log("(4) Settings");
 	console.log("(9) Quit");
 };
 
 displayMenu();
 
+var desplayMenu1;
 
+displayMenu1 = function () {
+	console.log("Pick Up a movie:");
+	console.log("(1) Home");
+	console.log("(2) Theatre");
+	console.log("(3) Booking");
+        console.log("(4) Settings");
+	console.log("(9) Quit");
+};
+
+displayMenu1();
 
 /* Further Adventures
  *
