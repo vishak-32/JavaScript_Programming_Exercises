41ef0be5ab0c8a63a5a0f75f66e3ae4e5df6ea33
vishak-32 <spvishak8@gmail.com>
2024-07-14 19:52:34 +0530

    Update Ch3_pgm_01.js

--- 03_JS_Objects/Ch3_pgm_01.js
+++ 03_JS_Objects/Ch3_pgm_01.js
@@ -8,6 +8,21 @@ bookAuthor = "J. R. R. Tolkien";
 
 console.log(bookTitle + " by " + bookAuthor);
 
+var bookTitle1;
+var bookAuthor1;
+
+bookTitle1="To Kill a Mockingbird;
+bookAuthor1="Harper Lee";
+
+console.log(bookTitle1+" by "+bookAuthor1);
+
+var bookTitle2;
+var bookAuthor2;
+
+bookTitle2="Brave New World";
+bookAuthor2="Aldous Huxley";
+
+console.log(bookTitle2+" by "+bookAuthor2);
 
 
 /* Further Adventures
