1bd3da2d4ee6d9f4b9ad38a226d4eb4fdb30074d
vishak-32 <spvishak8@gmail.com>
2024-07-14 19:01:03 +0530

    Update Ch2_pgm_03.js

--- 02_JS_variables/Ch2_pgm_03.js
+++ 02_JS_variables/Ch2_pgm_03.js
@@ -1,10 +1,25 @@
 
+// Using a variable
+
 var score;
-score = 325;
+score = 525;
 console.log(score);
 var score2;
-score2 = 150;
+score2 = 1503;
 console.log(score2);
 
 
+
+/* Further Adventures
+ *
+ * 1) Change the value assigned to score.
+ *    Run the program again.
+ *
+ * 2) Declare a second variable, maybe score2.
+ *
+ * 3) Assign your variable a value.
+ *
+ * 4) Use console.log to display your variable on the console.
+ *
+ */
 
