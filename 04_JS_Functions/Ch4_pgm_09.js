dfed4e26135ca07e159f5d2c55f30e396dc45bc3
vishak-32 <spvishak8@gmail.com>
2024-07-14 20:39:41 +0530

    Update Ch4_pgm_09.js

--- 04_JS_Functions/Ch4_pgm_09.js
+++ 04_JS_Functions/Ch4_pgm_09.js
@@ -3,14 +3,19 @@
 var showMovieInfo;
 
 showMovieInfo = function () {
-  console.log("Movie information for " + movie.title);
-  console.log("------------------------------");
-  console.log("Actors: " + movie.actors);
-  console.log("Directors: " + movie.directors);
-  console.log("------------------------------");
+    console.log("Movie information for " + movie.title);
+    console.log("------------------------------");
+    console.log("Actors: " + movie.actors);
+    console.log("Directors: " + movie.directors);
+    console.log("------------------------------");
 };
+var movie;
+movie = {};
+movie.title = "Inception";
+movie.actors = "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page";
+movie.directors = "Christopher Nolan";
 
-
+showMovieInfo(); 
 
 /* Further Adventures
  *
