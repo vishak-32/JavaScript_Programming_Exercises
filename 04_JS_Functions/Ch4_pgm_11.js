06004e8e68023a06a3e9f7ceb8698639c3e25af5
vishak-32 <spvishak8@gmail.com>
2024-07-14 20:41:32 +0530

    Update Ch4_pgm_11.js

--- 04_JS_Functions/Ch4_pgm_11.js
+++ 04_JS_Functions/Ch4_pgm_11.js
@@ -1,88 +1,39 @@
 // Using the same function with multiple objects
 
-var movie1;
-var movie2;
-var movie3;
-var movie;
-var showMovieInfo;
+var sale1;
+var sale2;
+var sale3;
+var sale;
+var calculateTax;
+var displaySale;
+var calculateAndDisplayTax;
 
-movie1 = {
-    title: "Inside Out",
-    actors: "Amy Poehler, Bill Hader",
-    directors: "Pete Doctor, Ronaldo Del Carmen"
+sale1 = { price: 140, taxRate: 15 };
+sale2 = { price: 40, taxRate: 10 };
+sale3 = { price: 120, taxRate: 20 };
+sale4 = { price: 200, taxRate: 18 };
+
+calculateAndDisplayTax = function () {
+    sale.tax = sale.price * sale.taxRate / 100;
+    sale.total = sale.price + sale.tax;
+    
+    console.log("price = $" + sale.price);
+    console.log("tax @ " + sale.taxRate + "% = $" + sale.tax);
+    console.log("total cost = $" + sale.total);
+    console.log("------------------------------"); 
 };
 
-movie2 = {
-    title: "Spectre",
-    actors: "Daniel Craig, Christoph Waltz",
-    directors: "Sam Mendes"
-};
+sale = sale1;
+calculateAndDisplayTax();
 
-movie3 = {
-    title: "Star Wars: Episode VII - The Force Awakens",
-    actors: "Harrison Ford, Mark Hamill, Carrie Fisher",
-    directors: "J.J.Abrams"
-};
+sale = sale2;
+calculateAndDisplayTax();
 
-showMovieInfo = function () {
-    console.log("Movie information for " + movie.title);
-    console.log("------------------------------");
-    console.log("Actors: " + movie.actors);
-    console.log("Directors: " + movie.directors);
-    console.log("------------------------------");
-};
+sale = sale3;
+calculateAndDisplayTax();
 
-movie = movie1;
-showMovieInfo();
-
-movie = movie2;
-showMovieInfo();
-
-movie = movie3;
-showMovieInfo();
-
-var question1;
-var question2;
-var question3;
-var question;
-var showQuestion;
-
-question1 = {
-    questionText: "What is the capital of India?",
-    options: ["A) Coimbatore", "B) Maharashatra", "C) Delhi", "D) Gujarat"],
-    correctAnswer: "C"
-};
-
-question2 = {
-    questionText: "Which is known as Scripting language?",
-    options: ["A) Javascript", "B) HTML", "C) CSS", "D) Java"],
-    correctAnswer: "A"
-};
-
-question3 = {
-    questionText: "Who wrote 'Wings of fire'?",
-    options: ["A) Abdul Kalam", "B) Oscar Wilde", "C) Ruskin Bond", "D) R. K. Narayanan"],
-    correctAnswer: "A"
-};
-
-showQuestion = function () {
-    console.log("Question: " + question.questionText);
-    console.log("Options:");
-    for (var i = 0; i < question.options.length; i++) {
-        console.log(question.options[i]);
-    }
-    console.log("------------------------------");
-};
-
-question = question1;
-showQuestion();
-
-question = question2;
-showQuestion();
-
-question = question3;
-showQuestion();
-
+sale = sale4;
+calculateAndDisplayTax();
 
 
 /* Further Adventures
