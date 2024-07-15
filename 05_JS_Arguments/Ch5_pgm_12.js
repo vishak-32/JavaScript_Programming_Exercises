bb29c300a858972bb19a668576bf290728c1a40a
vishak-32 <spvishak8@gmail.com>
2024-07-15 19:32:26 +0530

    Update Ch5_pgm_12.js

--- 05_JS_Arguments/Ch5_pgm_12.js
+++ 05_JS_Arguments/Ch5_pgm_12.js
@@ -5,7 +5,7 @@ var player2;
 var showPlayerPlace;
 
 showPlayerPlace = function (playerName, playerPlace) {
-    console.log(playerName + " is in " + playerPlace);
+    console.log(playerName.substring(0, 1) + " is in " + playerPlace); 
 };
 
 player1 = {
@@ -18,6 +18,34 @@ player2 = {
     name: "Dax",
     place: "The Old Library",
     health: 40
+};
+
+showPlayerPlace(player1.name, player1.place);
+showPlayerPlace(player2.name, player2.place);
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName.substring(0, 2) + " is in " + playerPlace);
+};
+
+showPlayerPlace(player1.name, player1.place);
+showPlayerPlace(player2.name, player2.place);
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName.substring(0, 3) + " is in " + playerPlace); 
+};
+
+showPlayerPlace(player1.name, player1.place);
+showPlayerPlace(player2.name, player2.place);
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName.substring(1, 2) + " is in " + playerPlace); 
+};
+
+showPlayerPlace(player1.name, player1.place);
+showPlayerPlace(player2.name, player2.place);
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName.substring(1, 3) + " is in " + playerPlace); 
 };
 
 showPlayerPlace(player1.name, player1.place);
