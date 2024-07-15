28d56d7c91363c512cfe23905684866acaa31ef5
vishak-32 <spvishak8@gmail.com>
2024-07-15 19:41:56 +0530

    Update Ch6_pgm_08.js

--- 06_JS_Return_values/Ch6_pgm_08.js
+++ 06_JS_Return_values/Ch6_pgm_08.js
@@ -2,6 +2,7 @@
 
 var getPlayerHealth;
 var getPlayerPlace;
+var showPlayerInfo;
 
 getPlayerHealth = function (playerName, playerHealth) {
     return playerName + " has health " + playerHealth;
@@ -11,8 +12,15 @@ getPlayerPlace = function (playerName, playerPlace) {
     return playerName + " is in " + playerPlace;
 };
 
-console.log(getPlayerHealth("Kandra", 50));
+showPlayerInfo = function (playerName, playerHealth, playerPlace) {
+    console.log(getPlayerHealth(playerName, playerHealth));
+    console.log(getPlayerPlace(playerName, playerPlace));
+};
+
+console.log(getPlayerHealth("Kandra", 50)); 
 console.log(getPlayerPlace("Kandra", "The Dungeon of Doom"));
+
+showPlayerInfo("Dax", 50, "The Bath"); 
 
 
 
