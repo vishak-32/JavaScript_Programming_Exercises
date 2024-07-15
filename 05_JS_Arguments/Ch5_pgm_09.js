39e3cc3c0634411b90d05e72d3cb2248eb22d93c
vishak-32 <spvishak8@gmail.com>
2024-07-15 19:31:07 +0530

    Update Ch5_pgm_09.js

--- 05_JS_Arguments/Ch5_pgm_09.js
+++ 05_JS_Arguments/Ch5_pgm_09.js
@@ -3,11 +3,14 @@
 var showPlayerHealth;
 
 showPlayerHealth = function (playerName, playerHealth) {
-    console.log(playerName + " has health " + playerHealth);
+    var healthInfo;
+    healthInfo = playerName + ": health " + playerHealth;
+    console.log(healthInfo);
 };
 
 showPlayerHealth("Kandra", 50);
 showPlayerHealth("Dax", 40);
+
 
 
 
