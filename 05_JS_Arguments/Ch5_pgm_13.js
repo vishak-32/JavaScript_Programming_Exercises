0a1e58b42137c23136867b66e71d07c2b004b0ad
vishak-32 <spvishak8@gmail.com>
2024-07-15 19:32:57 +0530

    Update Ch5_pgm_13.js

--- 05_JS_Arguments/Ch5_pgm_13.js
+++ 05_JS_Arguments/Ch5_pgm_13.js
@@ -4,6 +4,8 @@ var showPlayerInfo;
 var showPlayerName;
 var showPlayerHealth;
 var showPlayerPlace;
+var showLine;
+var showBlankLine;
 
 showPlayerName = function (playerName) {
     console.log(playerName);
@@ -17,23 +19,30 @@ showPlayerPlace = function (playerName, playerPlace) {
     console.log(playerName + " is in " + playerPlace);
 };
 
-showPlayerInfo = function (playerName, playerPlace, playerHealth) {
+showLine = function () {
+    console.log("----------------------------");
+};
+
+showBlankLine = function () {
     console.log("");
+};
+
+showPlayerInfo = function (playerName, playerPlace, playerHealth) {
+    showBlankLine();
 
     showPlayerName(playerName);
 
-    console.log("----------------------------");
+    showLine();
 
     showPlayerPlace(playerName, playerPlace);
     showPlayerHealth(playerName, playerHealth);
 
-    console.log("----------------------------");
-    console.log("");
+    showLine();
+    showBlankLine();
 };
 
 showPlayerInfo("Kandra", "The Dungeon of Doom", 50);
 showPlayerInfo("Dax", "The Old Library", 40);
-
 
 
 /* Further Adventures
