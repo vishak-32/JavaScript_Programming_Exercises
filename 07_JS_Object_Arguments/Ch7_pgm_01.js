94b380edb2b55e15a250735489262fb64b366a4c
vishak-32 <spvishak8@gmail.com>
2024-07-15 20:06:43 +0530

    Update Ch7_pgm_01.js

--- 07_JS_Object_Arguments/Ch7_pgm_01.js
+++ 07_JS_Object_Arguments/Ch7_pgm_01.js
@@ -1,6 +1,6 @@
 // Passing a function an object as an argument
 
-var planet1;
+var planet1, planet2;
 var getPlanetInfo;
 
 planet1 = {
@@ -11,12 +11,24 @@ planet1 = {
     sizeRank: 1
 };
 
+planet2 = {
+    name: "Mercury",
+    position: 1,
+    type: "Gas Giant",
+    radius: 3424,
+    sizeRank: 7
+};
+
 getPlanetInfo = function (planet) {
-    return planet.name + ": planet number " + planet.position;
+    return planet.name + 
+           ": planet number " + planet.position + 
+           ", type: " + planet.type + 
+           ", radius: " + planet.radius + " km" + 
+           ", size rank: " + planet.sizeRank;
 };
 
 console.log(getPlanetInfo(planet1));
-
+console.log(getPlanetInfo(planet2));
 
 
 /* Further Adventures
