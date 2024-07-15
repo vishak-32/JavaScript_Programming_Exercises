8c936fec4a9a1061d9a6a1e4d8adef8c122922b7
vishak-32 <spvishak8@gmail.com>
2024-07-15 19:39:34 +0530

    Update Ch6_pgm_05.js

--- 06_JS_Return_values/Ch6_pgm_05.js
+++ 06_JS_Return_values/Ch6_pgm_05.js
@@ -1,13 +1,13 @@
 // A function with three arguments
-
 var totalCost;
 
-totalCost = function (callOutCharge, costPerHour, numberOfHours) {
-	return callOutCharge + costPerHour * numberOfHours;
+totalCost = function (callOutCharge, costPerHour, numberOfHours, discount) {
+    return callOutCharge + costPerHour * numberOfHours - discount;
 };
 
-console.log("$" + totalCost(30, 40, 3));
+console.log("$" + totalCost(30, 40, 12, 0)); 
 
+console.log("$" + totalCost(30, 40, 3, 20)); 
 
 
 /* Further Adventures
