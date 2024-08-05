0971a483a874c7df14af5c20b2a28c753acb2d2c
vishak-32 <spvishak8@gmail.com>
2024-08-05 20:01:10 +0530

    Update Ch8_pgm_07.js

--- 08_JS_Arrays/Ch8_pgm_07.js
+++ 08_JS_Arrays/Ch8_pgm_07.js
@@ -6,14 +6,26 @@ var showInfo;
 items = [
   "The Pyramids",
   "The Grand Canyon",
-  "Bondi Beach"
+  "Bondi Beach",
+  "Effiel Tower",
+  "Taj Mahal",
+  "The Great Wall of China"
 ];
 
 showInfo = function (itemToShow) {
-    console.log(itemToShow);
+    console.log(itemToShow + " has " + itemToShow.length + " of letters");
 };
 
 items.forEach(showInfo);
+
+var totalLetters = function(itemsArray){
+    var total = 0;
+    itemsArray.forEach(function(item){
+        total += item.length;
+    });
+    return total;
+};
+console.log("Total number of letters in the array is " + totalLetters(items));
 
 
 
