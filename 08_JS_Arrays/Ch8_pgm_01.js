73f870c178ee6cf6795605d1f3459f094cd735f4
vishak-32 <spvishak8@gmail.com>
2024-08-05 19:56:51 +0530

    Update Ch8_pgm_01.js

--- 08_JS_Arrays/Ch8_pgm_01.js
+++ 08_JS_Arrays/Ch8_pgm_01.js
@@ -3,13 +3,13 @@
 var scores;
 var names;
 
-scores = [ 3, 1, 8, 2 ];
-names = [ "Kandra", "Dax", "Blinky" ];
+scores = [ 3, 1, 8, 2, 5];
+names = [ "Kandra", "Dax", "Blinky", "Browiee"];
+var akaScores = scores;
 
 console.log(scores);
 console.log(names);
-
-
+console.log(akaScores);
 
 /* Further Adventures
  *
